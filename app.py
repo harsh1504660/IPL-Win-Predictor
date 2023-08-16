2b944d0a98a1328502e67d1156c4d39d4abbb570
Harsh Joshi <harshsmj1504@gmail.com>
2023-08-16 22:39:39 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+st.title('IPL WIN PREDICTOR')
+
+team = ['Sunrisers Hyderabad',
+ 'Mumbai Indians',
+ 'Royal Challengers Bangalore',
+ 'Kolkata Knight Riders',
+ 'Kings XI Punjab',
+ 'Chennai Super Kings',
+ 'Rajasthan Royals',
+ 'Delhi Capitals']
+
+cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi',
+       'Chandigarh', 'Jaipur', 'Chennai', 'Cape Town', 'Port Elizabeth',
+       'Durban', 'Centurion', 'East London', 'Johannesburg', 'Kimberley',
+       'Bloemfontein', 'Ahmedabad', 'Cuttack', 'Nagpur', 'Dharamsala',
+       'Visakhapatnam', 'Pune', 'Raipur', 'Ranchi', 'Abu Dhabi',
+       'Sharjah', 'Mohali', 'Bengaluru']
+
+pipe = pickle.load(open('pipe.pkl','rb'))
+
+col1 , col2 = st.columns(2)
+
+with col1 :
+    batting_team = st.selectbox('Select The Batting Team',sorted(team))
+
+with col2 :
+    bowling_team = st.selectbox('Select The Bowling Team',sorted(team,reverse=True))
+
+selected_city = st.selectbox('Select The Host City',sorted(cities))
+
+target = st.number_input('Target')
+
+col3 , col4,col5 = st.columns(3)
+with col3:
+    score = st.number_input('Current Score')
+with col4:
+    overs = st.number_input('Overs')
+with col5:
+    Wickets = st.number_input('Wickets')
+
+if st.button('Predict Probablity'):
+    runs_left = target - score
+    balls_left = 120 - (overs*6)
+    wickets = 10 - Wickets
+    crr = score/overs
+    rrr = (runs_left*6)/balls_left
+
+    input_df = pd.DataFrame({'batting_team':[batting_team],'bowling_team':[bowling_team],
+                  'city':[selected_city],'runs_left':[runs_left],'balls_left':[balls_left],
+                  'wickets':[wickets],'total_runs_x':[target],'crr':[crr],'rrr':[rrr]})
+    
+    result = pipe.predict_proba(input_df)
+    loss = result[0][0]
+    win = result[0][1]
+    st.header(batting_team+'- '+'%.2f'%(win*100)+'%')
+    st.header(bowling_team+'- '+'%.2f' % (loss*100)+'%')
